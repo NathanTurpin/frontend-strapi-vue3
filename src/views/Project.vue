af030cff7071527ea2e67bcdaa339dd4e38fef2e
Antoine CHAMBRIN <antoinechambrin@gmail.com>
2022-01-18 22:35:00 +0100

    structure projects statique

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,5 +1,66 @@
 <template>
-  <div class="project">
-    <h1>This is an project list page</h1>
+  <div class="projectNav">
+    <div class="temporaryStatic">
+      <h3>Tous les projets</h3>
+      <h3>Bureaux</h3>
+      <h3>Restaurant</h3>
+      <h3>Médical</h3>
+      <h3>Industrie</h3>
+    </div>
+  </div>
+  <div class="projectItems">
+    <div class="temporaryStatic">
+      <div class="projectId">
+        01
+      </div>
+      <div class="projectName">
+        <div class="temporaryStatic">
+          <router-link :to="{ path: '/projects/details' }">
+              Nom Projet
+          </router-link>
+        </div>
+       <!-- <router-link :to="{ path: '/projects/' + project.id }">{{ project.attributes.projectName }}</router-link> --> <!-- pour plustard -->
+      </div>
+      <div class="projectImage">
+        <div class="backgroundImage">
+          <img src="" alt="" srcset="">
+        </div>
+        <div class="displayImage">
+          <img src="" alt="" srcset="">
+        </div>
+      </div>
+    </div>
+
+    <div class="temporaryStatic">
+      <div class="projectId">
+        02
+      </div>
+      <div class="projectName">
+        <div class="temporaryStatic">
+          <router-link :to="{ path: '/projects/details' }">
+              Nom Projet
+          </router-link>
+        </div>
+       <!-- <router-link :to="{ path: '/projects/' + project.id }">{{ project.attributes.projectName }}</router-link> --> <!-- pour plustard -->
+      </div>
+      <div class="projectImage">
+        <div class="backgroundImage">
+          <img src="" alt="" srcset="">
+        </div>
+        <div class="displayImage">
+          <img src="" alt="" srcset="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
