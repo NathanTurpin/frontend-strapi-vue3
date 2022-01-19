3ac9b95ae03a513055c293ea755d4f8695d1f46f
Antoine CHAMBRIN <antoinechambrin@gmail.com>
2022-01-19 10:55:09 +0100

    avancement structure project details

--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -1,5 +1,89 @@
 <template>
   <div class="projectdetail">
-    <h1>This is an about page</h1>
+    <div class="projectHeader">
+      <div class="projectTitle">
+        <h1>Nom projet</h1>
+      </div>
+      <div class="projectDescription">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+           incididunt ut labore et dolore magna ali. 
+        </p>
+      </div>
+      <div class="projectDetails">
+        <div class="projectDate">
+          Projet réalisé en 2017
+        </div>
+        <div class="projectPlace">
+          Arras, 62000
+        </div>
+      </div>
+      <div class="projectHeaderImage">
+        <img src="" alt="" srcset="">
+      </div>
+    </div>
+
+    <div class="projectSection">
+      <div class="projectTitleSection">
+        <h2>Title section</h2>
+      </div>
+      <div class="ProjectSectionText">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum 
+           dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+           ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+
+    <div class="projectImageSection">
+      <img src="" alt="" srcset="">
+    </div>
+
+    <div class="projectDualImageSection">
+      <div class="leftImage">
+        <img src="" alt="">
+      </div>
+      <div class="rightImage">
+        <img src="" alt="">
+      </div>
+    </div>
+
+    <div class="projectSection">
+      <div class="projectTitleSection">
+        <h2>Title section</h2>
+      </div>
+      <div class="ProjectSectionText">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum 
+           dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+           ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+
+    <div class="projectImageSection">
+      <img src="" alt="" srcset="">
+    </div>
+
+    <div class="projectDescriptionSection">
+      
+    </div>
+
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
