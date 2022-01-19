b61a8e36b355951dd8bf6ce9b9dbde95ff9574ba
Antoine CHAMBRIN <antoinechambrin@gmail.com>
2022-01-19 21:58:15 +0100

    avancement home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,121 @@
 <template>
-  <div>HOME</div>
+  <div class="home">
+    <div class="left">
+      <div class="header">
+        <h3>CONCENTREZ-VOUS SUR VOTRE Métier</h3>
+        <h3> contractant général</h3>
+      </div>
+      <div class="mainTitle">
+        <h1>
+          Nous créons votre espace idéal<span class="importantColor">*</span>
+        </h1>
+      </div>
+      <div class="spec">
+        Nous ne réalisons pas des bâtiments professionnels.
+         C’est différent. Nous proposons une experience de création holistique
+          pour des lieux de vie et de travail qui changent tout.
+      </div>
+    </div>
+    <div class="right">
+      <div class="imageScroll">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+      </div>
+      <div class="text1">
+        attirer et retenir les talents ?
+      </div>
+      <div class="text1">
+        insuffler un état d'esprit ?
+      </div>
+    </div>
+    <div class="whiteBox">
+      <div class="whiteBoxText">
+        <div class="line"></div>
+        <div class="importantColor">
+          Comment
+        </div>
+      </div>
+    </div>
+    <div class="yellowBox">
+      <div class="topLeftCorner">
+        <h2>
+          Nous vous aidons à faire la différence
+        </h2>
+      </div>
+      <div class="botLeftCorner">
+        <p>
+          Pour autant que qu’ils soient de ce type :  avoir un lieu qui vous ressemble et 
+          dont vous êtes fière, un lieu qui rassemble autour de votre vision et de vos valeurs.
+        </p>
+      </div>
+      <div class="midImage">
+        <div class="roundImage">
+          <img src="" alt="" srcset="">
+        </div>
+      </div>
+      <div class="botRightCorner">
+        <h2>
+          Et faire de vos rêves, une réalité <span class="importantColor">*</span>
+        </h2>
+      </div>
+    </div>
+    <div class="vision">
+      <h3>Notre vision</h3>
+      <div class="visionDescription">
+          <h2>
+            Nous avons à coeur de créer des <span class="importantColor">lieux  attirants et inspirants</span> dans le respect de l’environnement qui rendent <span class="importantColor">heureux</span> les utilisateurs. 
+          </h2>
+      </div>
+    </div>
+    <div class="realisations">
+      <div class="realisationSummary">
+        <h3>nos réalisations</h3>
+        <div class="realisationItems">
+          <h2>Bureaux</h2>
+          <h2>Restaurants</h2>
+          <h2>Médical</h2>
+          <h2>Industrie</h2>
+        </div>
+        <div class="seeProjects">
+          Voir tous nos projets
+        </div>
+      </div>
+      <div class="realisationImage">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+      </div>
+    </div>
+    <div class="horizontalScroll">
+      <div class="page1">
+        <div class="horizontalHeader">
+          <h3>da aalto code</h3>
+        </div>
+      </div>
+      <div class="page2">
+        <div class="horizontalHeader">
+          <h3>da aalto code</h3> 
+        </div>
+      </div>
+      <div class="page3">
+        <div class="horizontalHeader">
+          <h3>da aalto code</h3> 
+        </div>
+      </div>
+      <div class="page4">
+        <div class="horizontalHeader">
+          <h3>da aalto code</h3>
+        </div>
+      </div>
+      <div class="page5">
+        <div class="horizontalHeader">
+          <h3>da aalto code</h3>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
